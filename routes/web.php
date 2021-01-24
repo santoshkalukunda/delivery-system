239255abad5a8120ad3addc05db9e7cbaa3e80a4
santoshkalukunda <santoshkalukunda@gmail.com>
2021-01-24 11:32:21 +0545

    some changes

--- routes/web.php
+++ routes/web.php
@@ -23,21 +23,29 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+    Route::view('dashbord', 'dashbord');
+    Route::resource('cities', CityController::class);
+    Route::resource('branches', BranchController::class);
+    Route::get('customers/find', [CustomerController::class, 'view'])->name('customers.view');
+    Route::get('customers/new', [CustomerController::class, 'find'])->name('customers.find');
+    Route::resource('customers', CustomerController::class);
+    Route::get('product-orders/{customer}', [ProductOrderController::class, 'create'])->name('product-orders.create');
+    Route::post('product-orders/{customer}', [ProductOrderController::class, 'store'])->name('product-orders.store');
+    Route::get('product-orders', [ProductOrderController::class, 'index'])->name('product-orders.index');
+    Route::delete('product-orders/{productOrder}', [ProductOrderController::class, 'destroy'])->name('product-orders.destroy');
+    Route::get('product-orders/{productOrder}/show', [ProductOrderController::class, 'show'])->name('product-orders.show');
+    Route::get('product-orders/{productOrder}/edit', [ProductOrderController::class, 'edit'])->name('product-orders.edit');
+    Route::put('product-orders/{productOrder}', [ProductOrderController::class, 'update'])->name('product-orders.update');
+    // Route::resource('product-orders', ProductOrderController::class);
+});
+Route::group(['middleware' => ['role:user|admin']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::resource('cities', CityController::class);
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
-Route::view('dashbord', 'dashbord');
-
-Route::resource('cities', CityController::class);
-Route::resource('branches', BranchController::class);
-Route::get('customers/find', [CustomerController::class,'view'])->name('customers.view');
-Route::get('customers/new', [CustomerController::class,'find'])->name('customers.find');
-Route::resource('customers', CustomerController::class);
-Route::get('product-orders/{customer}', [ProductOrderController::class,'create'])->name('product-orders.create');
-Route::post('product-orders/{customer}', [ProductOrderController::class,'store'])->name('product-orders.store');
-Route::get('product-orders', [ProductOrderController::class,'index'])->name('product-orders.index');
-Route::delete('product-orders/{productOrder}', [ProductOrderController::class,'destroy'])->name('product-orders.destroy');
-Route::get('product-orders/{productOrder}/show', [ProductOrderController::class,'show'])->name('product-orders.show');
-Route::get('product-orders/{productOrder}/edit', [ProductOrderController::class,'edit'])->name('product-orders.edit');
-Route::put('product-orders/{productOrder}', [ProductOrderController::class,'update'])->name('product-orders.update');
-// Route::resource('product-orders', ProductOrderController::class);
+Route::group(['middleware' => ['role:delivery_agent|user|admin']], function () {
+    Route::view('delivery-agent','delivery-agent.home')->name('delivery-agent');
+});
